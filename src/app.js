78261af5c10f05266d74acdd71e4ea2475eb0834
AngieMonroe <ame.monroy@gmail.com>
2018-08-22 10:33:13 -0500

    Alcance dos, Mostrando tres categorias y su información en pantalla

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const movieDom = document.querySelector("input");
+const btnBatman = document.getElementById('btn-batman');
+const btnWoman = document.getElementById('btn-woman');
+const btnRobin = document.getElementById('btn-robin');
+const series = document.getElementById('serie');
 
 movieDom.addEventListener("keypress", (event) =>{
     let key = event.which || event.keyCode;
@@ -8,3 +12,26 @@ movieDom.addEventListener("keypress", (event) =>{
         getData(movie);
     }
 });
+
+btnBatman.addEventListener('click', (event) => {
+    getData('Batman')
+})
+
+btnWoman.addEventListener('click', (event) => {
+    getData('Wonder Woman')
+})
+
+btnRobin.addEventListener('click', (event) => {
+    getData('Robin Hood')
+})
+
+const renderInfo = (data) => {
+    serie.innerHTML = '';
+    data = data.Search
+    for(let i = 0; i < data.length; i++){
+        series.innerHTML += `
+        <img src=${data[i].Poster}>
+        <p>${data[i].Title}</p>
+        `
+    }
+}
