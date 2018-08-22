026d75b1999a7ccf70d01dfafb76e2c03c4756cc
AngieMonroe <ame.monroy@gmail.com>
2018-08-22 15:50:44 -0500

    Tercer alcance, funcionalidad del modal

--- src/app.js
+++ src/app.js
@@ -30,8 +30,31 @@ const renderInfo = (data) => {
     data = data.Search
     for(let i = 0; i < data.length; i++){
         series.innerHTML += `
-        <img src=${data[i].Poster}>
-        <p>${data[i].Title}</p>
-        `
+        <!-- Button trigger modal -->
+        <a type="button" data-toggle="modal" data-target="#${data[i].imdbID}"> <img src=${data[i].Poster}>
+        </a>
+        <p>${data[i].Title}
+          
+          <!-- Modal -->
+          <div class="modal fade" id="${data[i].imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalCenterTitle">${data[i].Title}</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>${data[i].Type}</p>
+                  <p>${data[i].Year}</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
     }
 }
--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
-window.getData = (movie) => {
-    fetch (`http://www.omdbapi.com/?s=${movie}&apikey=3356e4a4&type=series`)
+window.getData = (serie) => {
+    fetch (`http://www.omdbapi.com/?s=${serie}&apikey=3356e4a4&type=series`)
     .then (response => response.json())
     .then (data => {
         console.log(data)
